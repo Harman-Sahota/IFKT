2372c1f126692e6de2c48f609710e08463891682
Harman-Sahota <10harmansahota@gmail.com>
2024-01-05 00:55:03 -0500

    animation added

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -5,9 +5,32 @@ import styles from '../Pages/home.module.css';
 
 const SiteNavbar = () => {
   const scrollToSection = (className) => {
-    const element = document.getElementsByClassName(className);
-    if (element) {
-      element.scrollIntoView({ behavior: 'smooth' });
+    const elements = document.getElementsByClassName(className);
+  
+    if (elements.length > 0) {
+      const element = elements[0];
+      const duration = 1000; // Adjust as needed
+      const start = window.scrollY || window.pageYOffset;
+      const target = element.getBoundingClientRect().top + start;
+      const startTime = performance.now();
+  
+      const scroll = (currentTime) => {
+        const progress = (currentTime - startTime) / duration;
+        const easedProgress = easeInOutQuad(progress);
+        const newPosition = start + (target - start) * easedProgress;
+  
+        window.scrollTo(0, newPosition);
+  
+        if (progress < 1) {
+          requestAnimationFrame(scroll);
+        }
+      };
+  
+      const easeInOutQuad = (t) => (t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t);
+  
+      requestAnimationFrame(scroll);
+    } else {
+      console.log('Element not found:', className);
     }
   };
 
